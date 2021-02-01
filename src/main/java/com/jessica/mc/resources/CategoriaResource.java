e4081fd467b04eb7dc02c6c99377cae0d8d7b16e
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-02-01 20:27:44 -0300

    Including Repository and Service to Category

--- src/main/java/com/jessica/mc/resources/CategoriaResource.java
+++ src/main/java/com/jessica/mc/resources/CategoriaResource.java
@@ -1,10 +1,14 @@
 package com.jessica.mc.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jessica.mc.domain.Categoria;
+import com.jessica.mc.services.CategoriaService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,17 +17,15 @@ import java.util.List;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/jessica/mc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.jessica.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jessica.mc.domain.Categoria;
+import com.jessica.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null);
+	}
+
+}
