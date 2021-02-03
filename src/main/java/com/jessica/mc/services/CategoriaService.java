364f77a10edc598e54098bc03271596295c1fac1
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-02-03 20:29:25 -0300

    Adjustment on endpoint

--- src/main/java/com/jessica/mc/services/CategoriaService.java
+++ src/main/java/com/jessica/mc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jessica.mc.domain.Categoria;
 import com.jessica.mc.repositories.CategoriaRepository;
+import com.jessica.mc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
