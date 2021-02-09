65f65e709a263e6e65f7e2802d1d44ce84ab902a
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-02-09 13:57:38 -0300

    Endpoint/clientes/{id}

---
+++ src/main/java/com/jessica/mc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.jessica.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jessica.mc.domain.Cliente;
+import com.jessica.mc.repositories.ClienteRepository;
+import com.jessica.mc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
