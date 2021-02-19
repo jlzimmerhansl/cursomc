f88fe2c9352c652f10d7ad076755ab6d5c4fd020
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-02-19 11:47:26 -0300

    Endpoint /pedidos/id

---
+++ src/main/java/com/jessica/mc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.jessica.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jessica.mc.domain.Pedido;
+import com.jessica.mc.repositories.PedidoRepository;
+import com.jessica.mc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
