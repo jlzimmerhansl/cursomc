8a3643963b57a723b6d629d54aec066b05d1f26c
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-02-19 11:57:48 -0300

    Update: using only JsonIgnore

--- src/main/java/com/jessica/mc/domain/Produto.java
+++ src/main/java/com/jessica/mc/domain/Produto.java
@@ -10,14 +10,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.JoinColumn;
 
 @Entity
 public class Produto implements Serializable  {
@@ -30,7 +28,7 @@ public class Produto implements Serializable  {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
